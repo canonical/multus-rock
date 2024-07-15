fe7167286f14fcf1103683379910f27e7ed0dfd2
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2024-07-15 08:24:45 +0000

    Update tests
    We're updating the tests, following the same pattern used by the
    other rocks.
    
    The integration tests ensure that every supported version can be
    deployed using helm charts, while the sanity tests verify the
    image filesystem and executables.

--- tests/sanity/test_multus_v4_0_2.py
+++ tests/sanity/test_multus_v4_0_2.py
@@ -1,36 +1,50 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
-#
-#
 
-import random
-import pytest
-import string
 import subprocess
+import os
 
 from charmed_kubeflow_chisme.rock import CheckRock
 
 
-def check_image_paths(image, paths):
+def ensure_image_contains_paths(image, paths):
     for path in paths:
         subprocess.run(
             [
-                "docker", "run", image,
+                "docker", "run", "--rm", image,
                 "ls", "-l", path
             ],
             check=True,
         )
 
 
-def test_rock():
+def test_entrypoint_helpstring():
+    image = os.getenv("ROCK_MULTUS_V4_0_2")
+    assert image is not None, "ROCK_MULTUS_V4_0_2 is not set"
+    # "/thin_entrypoint --help" shows the help string but has a
+    # non-zero exit code (1).
+    docker_run = subprocess.run(
+        ["docker", "run", "--rm", "--entrypoint",
+         "/thin_entrypoint", image, "--help"],
+        capture_output=True,
+        check=False,
+        text=True,
+    )
+    assert (
+        "--multus-conf-file string" in docker_run.stderr
+    )
+
+
+def test_image_files():
     """Test rock."""
-    check_rock = CheckRock("rockcraft.yaml")
+    check_rock = CheckRock(
+        os.path.dirname(__file__) + "/../../v4.0.2/rockcraft.yaml")
     rock_image = check_rock.get_name()
     rock_version = check_rock.get_version()
     LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
 
     # check rock filesystem
-    check_image_paths(
+    ensure_image_contains_paths(
         LOCAL_ROCK_IMAGE,
         [
             "/install_multus",
@@ -42,3 +56,4 @@ def test_rock():
             "/usr/src/multus-cni/bin/multus-daemon",
             "/usr/src/multus-cni/bin/multus-shim",
         ])
+
